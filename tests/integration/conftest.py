a3a0ae9547e2eea09be3e4068a6820bb798a8867
Li Lin <guru.lin@gmail.com>
2022-02-23 15:17:46 +0800

    using IAM signature when calling API during integration test. remove cognito related functionality.

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,11 +1,8 @@
-import base64
-import json
 import os
 
 import boto3
 import pytest
 from botocore.exceptions import ClientError
-from pycognito import Cognito
 
 
 @pytest.fixture(scope="session", autouse=True)
@@ -22,31 +19,3 @@ def api_base_url() -> str:
     stack_outputs = response["Stacks"][0]["Outputs"]
     api_outputs = [item for item in stack_outputs if "ApiGatewayToLambdaLambdaRestApiEndpoint" in item["OutputKey"]]
     return api_outputs[0]["OutputValue"]
-
-
-@pytest.fixture
-def cognito_access_token() -> str:
-    """
-    get an access token from AWS Cognito and then use it call HttpApi
-    with JWT authorizer
-
-    the authorizer needs the following setup in order to work:
-    1. the issuer URL should match the iss attribute in the token
-    2. the audience should match the scope in the token
-    """
-    pool_id = os.environ.get("COGNITO_POOL_ID", "")
-    client_id = os.environ.get("COGNITO_CLIENT_ID", "")
-    client_secret = os.environ.get("COGNITO_CLIENT_SECRET", "")
-    credential = os.environ.get("COGNITO_LOGIN_CREDENTIAL", "")
-    if not all([credential, client_id, client_secret, pool_id]):
-        raise Exception("please specifcy set Cognito parameters in environment variables")
-
-    cred_dict = json.loads(credential)
-    user = Cognito(pool_id, client_id, client_secret=client_secret, username=cred_dict["username"])
-    user.authenticate(password=cred_dict["password"])
-    access_token = user.access_token
-
-    jwt = base64.standard_b64decode(access_token.split(".")[1])
-    print(f"{json.dumps(json.loads(jwt), indent=4)}")
-
-    return access_token
--- tests/integration/test_api_gateway.py
+++ tests/integration/test_api_gateway.py
@@ -1,11 +1,10 @@
 import requests
+import requests_aws_iam_auth
 
 
 def test_new_request(api_base_url):
-    headers = {"Authorization": "Bearer AAABBB"}
-    # headers = {"Authorization": f"Bearer {cognito_access_token}"}
-    url = f"{api_base_url}/customers/11223344/limits"
-    response = requests.post(url, json={"req_amount": 1000}, headers=headers)
+    auth = requests_aws_iam_auth.ApiGateway()
+    response = requests.post(f"{api_base_url}customers/11223344/limits", json={"req_amount": 1000}, auth=auth)
 
     assert response.status_code == 201
     assert response.json()["req_id"]
