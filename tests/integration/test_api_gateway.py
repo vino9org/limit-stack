be646e0bba8ae82af5388fe17a05bee3ec4ca14c
Li Lin <guru.lin@gmail.com>
2022-02-27 10:18:14 +0800

    use the more popular equests-aws4auth package to replace requests-aws-iam-auth

--- tests/integration/test_api_gateway.py
+++ tests/integration/test_api_gateway.py
@@ -1,10 +1,23 @@
+import boto3
+import pytest
 import requests
-import requests_aws_iam_auth
+from requests_aws4auth import AWS4Auth
 
 
-def test_new_request(api_base_url):
-    auth = requests_aws_iam_auth.ApiGateway()
-    response = requests.post(f"{api_base_url}customers/11223344/limits", json={"req_amount": 1000}, auth=auth)
+@pytest.fixture(scope="session")
+def api_auth() -> AWS4Auth:
+    session = boto3.Session()
+    credentials = session.get_credentials()
+    return AWS4Auth(
+        credentials.access_key,
+        credentials.secret_key,
+        session.region_name,
+        "execute-api",
+    )
+
+
+def test_new_request(api_auth, api_base_url):
+    response = requests.post(f"{api_base_url}customers/11223344/limits", json={"req_amount": 1000}, auth=api_auth)
 
     assert response.status_code == 201
     assert response.json()["req_id"]
