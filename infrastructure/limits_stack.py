6c83eec7633ece325725953c5b4f38cb9acb026b
Li Lin <guru.lin@gmail.com>
2022-04-23 15:32:05 +0800

    use NONE authorization type instead of default IAM

--- infrastructure/limits_stack.py
+++ infrastructure/limits_stack.py
@@ -34,6 +34,10 @@ class LimitsStack(Stack):
             name,
             api_gateway_props=apigateway.RestApiProps(
                 rest_api_name=name,
+                default_method_options=apigateway.MethodOptions(
+                    api_key_required=False,
+                    authorization_type=apigateway.AuthorizationType.NONE,
+                ),
                 endpoint_configuration=apigateway.EndpointConfiguration(
                     types=[apigateway.EndpointType.REGIONAL],
                 ),
